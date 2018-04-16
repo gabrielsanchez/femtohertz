0ce8f5fb7055a7d0454ca7d9cd135cc4376a5007
Gabriel <gabrielsz87@gmail.com>
2018-04-15 19:13:48 -0500

    Fix path issue

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import webapp2
 
 
 jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(os.path.dirname(__file__)))
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__))
 )
 
 jinja_environment.globals['year'] = datetime.now().year
@@ -13,9 +13,7 @@ jinja_environment.globals['year'] = datetime.now().year
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template(
-            'src/templates/index.html'
-        )
+        template = jinja_environment.get_template('/templates/index.html')
         self.response.out.write(template.render())
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
